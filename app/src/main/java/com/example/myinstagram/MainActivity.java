a100261168972af740a02d7a494132bdacb3ae63
Babu12345 <bwanyeki12@gmail.com>
2019-07-08 14:52:48 -0700

    Added Login and Signup activities

--- app/src/main/java/com/example/myinstagram/MainActivity.java
+++ app/src/main/java/com/example/myinstagram/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.myinstagram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText usernameInput;
+    private  EditText passwordInput;
+    private Button loginBtn;
+    private Button signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.username_et);
+        passwordInput = findViewById(R.id.password_et);
+        loginBtn = findViewById(R.id.login_btn);
+        signupBtn = findViewById(R.id.signup_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+
+                login(username, password);
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private void login(String username, String password){
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e==null){
+                    Log.d("LoginActivity", "Login Successful");
+                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                }else{
+                    Log.e("LoginActivity", "Login Failure");
+                    Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
